d0d0722f51dcc056b670b4faf9725baec3d8d2a9
mindcomconsulting <jitendrarathore948@gmail.com>
2016-03-06 15:05:34 +0530

    dsfa

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
 	def venue_detail
 		@function=FunctionDetail.all
 	end
-	
+
 	def function
 		@function=FunctionDetail.new
 	  render :layout=>false
@@ -30,6 +30,41 @@ class EventsController < ApplicationController
 		end
 		redirect_to :controller=>'events',:action=>'venue_detail'
 	end
+
+	def edit_function
+		puts"000000000000000000000000000000"
+		puts params
+		puts"000000000000000000000000000000"
+		@function = FunctionDetail.find_by(:id=>params[:id])
+		render :layout=>false
+	end
+
+	def update_function
+		puts"updateeeeeeeeeeeeeeeeee"
+		puts params
+		puts"updateeeeeeeeeeeeeeeeee"
+		function = FunctionDetail.find_by(:id=>params[:function][:id])
+		function.update(events_params)
+		if function
+			flash[:notice]="Successfully Updated"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'venue_detail'
+	end
+
+	def delete_function
+		function = FunctionDetail.find_by(:id=>params[:id])
+		if function.delete
+			flash[:notice]="Successfully Deleted"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'venue_detail'
+
+	end
+	
+	
 
 	def price
 		@price=Price.all
@@ -48,6 +83,39 @@ class EventsController < ApplicationController
 		@price.description=params[:price][:description]
 		if @price.save
 			flash[:notice]="Successfully Created"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'price'
+	end
+
+	def edit_price
+		puts"000000000000000000000000000000"
+		puts params
+		puts"000000000000000000000000000000"
+		@price = Price.find_by(:id=>params[:id])
+		@function=FunctionDetail.all.pluck(:name,:id)
+		render :layout=>false
+	end
+
+	def update_price
+		puts"updateeeeeeeeeeeeeeeeee"
+		puts params
+		puts"updateeeeeeeeeeeeeeeeee"
+		price = Price.find_by(:id=>params[:price][:id])
+		price.update(:function_id=>params[:price][:function_id],:price=>params[:price][:price],:description=>params[:price][:description])
+		if price
+			flash[:notice]="Successfully Updated"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'price'
+	end
+
+	def delete_price
+		price = Price.find_by(:id=>params[:id])
+		if price.delete
+			flash[:notice]="Successfully Deleted"
 		else
 			flash[:error]="There is some problem"
 		end
@@ -75,6 +143,38 @@ class EventsController < ApplicationController
 		redirect_to :controller=>'events',:action=>'venue'
 	end
 
+	def show_venue
+	end
+	def edit_venue
+		puts"000000000000000000000000000000"
+		puts params
+		puts"000000000000000000000000000000"
+		@venue_detail = Venue.find_by(:id=>params[:id])
+		render :layout=>false
+	end
+	def update_venue
+		puts"updateeeeeeeeeeeeeeeeee"
+		puts params
+		puts"updateeeeeeeeeeeeeeeeee"
+		venue = Venue.find_by(:id=>params[:venue_detail][:id])
+		venue.update(:name=>params[:venue_detail][:name],:address=>params[:venue_detail][:address])
+		if venue
+			flash[:notice]="Successfully Updated"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'venue'
+	end
+	def delete_venue
+		venue = Venue.find_by(:id=>params[:id])
+		if venue.delete
+			flash[:notice]="Successfully Deleted"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'venue'
+	end
+
 	def event_detail
 		@event_detail = EventDetail.all
 	end
@@ -84,23 +184,48 @@ class EventsController < ApplicationController
 		@function = FunctionDetail.all.pluck(:name,:id)
 	end
 	def create_event
-		add_event=EventDetail.new(:venu_id=>params[:event_detail][:venue_id],:phone_number=>params[:event_detail][:phone_number],
+		add_event=EventDetail.new(:venu_id=>params[:event_detail][:venu_id],:phone_number=>params[:event_detail][:phone_number],
 			:mobile_number=>params[:event_detail][:mobile_number],:capacity=>params[:event_detail][:capacity],
-			:function_id=>params[:event_detail][:function_id],:price_id=>params[:event_detail][:price])
-		if add_event.save
-			@event = EventDetail.find(add_event.id)
-			@image = add_event.images.new(:image=>params[:event_detail][:image],:title=>params[:event_detail][:title])
-    end
-  	if @image.save
+			:function_id=>params[:event_detail][:function_id],:price_id=>params[:event_detail][:price_id])
+		
+  	if add_event.save
 			flash[:notice]="Successfully Created"
 		else
 			flash[:error]="There is some problem"
 		end
-
-		 #add_event.images(params[:event_detail][:image])
-		 #add_event.save
 		redirect_to :controller=>'events',:action=>'event_detail'
 	end
+
+	def edit_event_detail
+		@event_detail = EventDetail.find_by(:id=>params[:id])
+		@venue = Venue.all.pluck(:name,:id)
+		@function = FunctionDetail.all.pluck(:name,:id)
+		render :layout=>false
+	end
+
+	def update_event_detail
+		add_event = EventDetail.find_by(:id=>params[:event_detail][:id])
+		updated_event = add_event.update(:venu_id=>params[:event_detail][:venu_id],:phone_number=>params[:event_detail][:phone_number],
+			:mobile_number=>params[:event_detail][:mobile_number],:capacity=>params[:event_detail][:capacity],
+			:function_id=>params[:event_detail][:function_id],:price_id=>params[:event_detail][:price_id])
+		if updated_event
+			flash[:notice]="Successfully Updated"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'event_detail'
+	end
+
+	def delete_event_detail
+		event = EventDetail.find_by(:id=>params[:id])
+		if event.delete
+			flash[:notice]="Successfully Deleted"
+		else
+			flash[:error]="There is some problem"
+		end
+		redirect_to :controller=>'events',:action=>'event_detail'
+	end
+
 
 	def venue_image
 		@venue_image = EventDetail.find_by(:id=>params[:id])
@@ -122,10 +247,15 @@ class EventsController < ApplicationController
 	def venue_address
 		if params[:function]=="by_venue"
 			venue_address = Venue.find_by(:id=>params[:venue_id])
-			@venue_address = venue_address[:address]
+			@venue_address = venue_address.address
 		else
-			price = Price.find_by(:id=>params[:function_id])
-			@venue_address = price[:price]
+			price_obj = Price.find_by(:id=>params[:function_id])
+			#venue_address = price.price
+			if price_obj.present?
+				@venue_address = price_obj.price
+			else
+				@venue_address = 'no data'
+			end
 		end
 		render :json=>{:venue_address=>@venue_address}
 	end
